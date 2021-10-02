821bd802719b8de0f94837821d26617d0dd645c6
irina.lebedeva <irina.lebedeva@travelline.ru>
2021-10-02 18:53:28 +0300

    Добавляет генерацию моковых данных

--- js/main.js
+++ js/main.js
@@ -1,26 +1,90 @@
-import {getRandomIntegerFromRange, getRandomFloatFromRange, ValidationError} from './utils/get-random-number.js';
+import {getRandomIntegerFromRange, getRandomFloatFromRange} from './utils/get-random-number.js';
 
-const errorHandler = (err) => {
-  //@todo тут будет какой-то обработчик
-  err.message;
+const ADVERTS_COUNT = 10;
+const MAX_ROOMS_COUNT = 7;
+const MIN_PRICE_VALUE = 500;
+const MAX_PRICE_VALUE = 100000;
+const MAX_GUESTS_COUNT = 10;
+const LAT_FROM_VALUE = 35.65000;
+const LAT_TO_VALUE = 35.70000;
+const LNG_FROM_VALUE = 139.70000;
+const LNG_TO_VALUE = 139.80000;
+const LOCATION_FRACTION_DIGITS = 5;
+const TEMPLATE_AVATAR_URL = 'img/avatars/user{{xx}}.png';
+const DATA_TITLE_DESCRIPTION = ['beautiful', 'comfortable', 'cool', 'wonderful', 'perfect'];
+const TEMPLATE_DESCRIPTION = '{{title}} with {{features}}';
+const DATA_TYPE = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const DATA_CHECKIN_CHECKOUT = ['12:00', '13:00', '14:00'];
+const DATA_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const DATA_PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
+
+Array.prototype.mixArr = function () {
+  // eslint-disable-next-line id-length
+  return this.map((i) => [Math.random(), i]).sort().map((i) => i[1]);
 };
 
-try {
-  getRandomIntegerFromRange(3, 15);
-} catch (err) {
-  if (err instanceof ValidationError) {
-    errorHandler(err);
-  } else {
-    throw err;
-  }
-}
+String.prototype.capitalize = function() {
+  return this.charAt(0).toUpperCase() + this.slice(1);
+};
 
-try {
-  getRandomFloatFromRange(4, 14, 7);
-} catch (err) {
-  if (err instanceof ValidationError) {
-    errorHandler(err);
-  } else {
-    throw err;
+const getAvatar = (advertId) => {
+  const avatarUrl = TEMPLATE_AVATAR_URL;
+  const getAvatarId = (currentAdvertId) => currentAdvertId < 10 ? (`0${currentAdvertId}`) : currentAdvertId;
+  return avatarUrl.replace('{{xx}}', getAvatarId(advertId));
+};
+
+const getType = () => DATA_TYPE[getRandomIntegerFromRange(0, DATA_TYPE.length - 1)];
+
+const getRandomArray = (dataArr) => (dataArr.length > 1) ?
+  dataArr.mixArr().slice(0, getRandomIntegerFromRange(1, dataArr.length)) : dataArr;
+
+const getTitle = (type) => `${DATA_TITLE_DESCRIPTION[getRandomIntegerFromRange(0, DATA_TITLE_DESCRIPTION.length - 1)]} ${type}`.capitalize();
+
+const getDescription = (title, features) =>
+  TEMPLATE_DESCRIPTION.replace('{{title}}', title).replace('{{features}}', getRandomArray(features).join(', '));
+
+const advertCard = (advertId) => {
+  const type = getType();
+  const title = getTitle(type);
+  const features = getRandomArray(DATA_FEATURES);
+  const lat = getRandomFloatFromRange(LAT_FROM_VALUE, LAT_TO_VALUE, LOCATION_FRACTION_DIGITS);
+  const lng = getRandomFloatFromRange(LNG_FROM_VALUE, LNG_TO_VALUE, LOCATION_FRACTION_DIGITS);
+
+  return {
+    author: {
+      avatar: getAvatar(advertId),
+    },
+    offer: {
+      title: title,
+      address: `${lat}, ${lng}`,
+      price: getRandomIntegerFromRange(MIN_PRICE_VALUE, MAX_PRICE_VALUE),
+      type: type,
+      rooms: getRandomIntegerFromRange(1, MAX_ROOMS_COUNT),
+      guests: getRandomIntegerFromRange(1, MAX_GUESTS_COUNT),
+      checkin: DATA_CHECKIN_CHECKOUT[getRandomIntegerFromRange(0, DATA_CHECKIN_CHECKOUT.length - 1)],
+      checkout: DATA_CHECKIN_CHECKOUT[getRandomIntegerFromRange(0, DATA_CHECKIN_CHECKOUT.length - 1)],
+      features: features,
+      description: getDescription(title, features),
+      photos: getRandomArray(DATA_PHOTOS),
+    },
+    location: {
+      lat: lat,
+      lng: lng,
+    },
+  };
+};
+
+const getAdvertCards = () => {
+  const mockAdvertCards = [];
+  // eslint-disable-next-line id-length
+  for (let i = 0; i < ADVERTS_COUNT; i++) {
+    mockAdvertCards[i] = advertCard(i+1);
   }
-}
+  return mockAdvertCards;
+};
+
+getAdvertCards();
