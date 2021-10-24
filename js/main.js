c2e0812457dd2424aa75392caecc35201cae4781
irina.lebedeva <irina.lebedeva@travelline.ru>
2021-10-24 21:14:29 +0300

    Добавляет маркеры после инициализации карты, генерирует разметку для попапа маркера по требованию

--- js/main.js
+++ js/main.js
@@ -8,24 +8,32 @@ import {getCurrentLang} from './utils/get-current-lang.js';
 
 const ADVERTS_COUNT = 10;
 
-const advertCards = createAdvertCards(ADVERTS_COUNT);
+const map = mapInitialize();
+
+const setMainMarker = () => {
+  const mainMarker = addMainMarker(map);
+  setAddress(mainMarker.getLatLng());
+  mainMarker.on('moveend', () => {
+    setAddress(mainMarker.getLatLng());
+  });
+};
+
+const setCommonMarkers = () => {
+  const advertCards = createAdvertCards(ADVERTS_COUNT);
+  advertCards.forEach((card) => {
+    const marker = addCommonMarker(map, [card.location.lat, card.location.lng]);
+    marker.bindPopup(() => generateCardMarkup(card));
+  });
+};
 
 setPageInactive();
 
-const map = mapInitialize();
-map.whenReady(setPageActive);
+map.whenReady(() => {
+  setPageActive();
 
-const mainMarker = addMainMarker(map);
-setAddress(mainMarker.getLatLng());
+  setMainMarker();
+  setCommonMarkers();
 
-mainMarker.on('moveend', () => {
-  setAddress(mainMarker.getLatLng());
+  loadLang(getCurrentLang());
+  formInitialize();
 });
-
-advertCards.forEach((card) => {
-  const marker = addCommonMarker(map, [card.location.lat, card.location.lng]);
-  marker.bindPopup(generateCardMarkup(card));
-});
-
-loadLang(getCurrentLang());
-formInitialize();
