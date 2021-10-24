9b21fe8e8f60e994e1df89089517a956398df2c6
irina.lebedeva <irina.lebedeva@travelline.ru>
2021-10-24 22:11:58 +0300

    Рефакторинг

--- js/set-page-state.js
+++ js/set-page-state.js
@@ -1,30 +1,17 @@
 const advertFormElement = document.querySelector('.ad-form');
-const advertFormElementsList = advertFormElement.querySelectorAll('input, select, textarea, button');
 const mapFiltersFormElement = document.querySelector('.map__filters');
-const mapFiltersFormElementsList = mapFiltersFormElement.querySelectorAll('input, select, textarea, button');
+const inputElementsList = document.querySelectorAll('.ad-form fieldset, .map__filters select, .map__filters fieldset');
 
 const setPageInactive = () => {
   advertFormElement.classList.add('ad-form--disabled');
-  advertFormElementsList.forEach((element) => {
-    element.setAttribute('disabled', 'disabled');
-  });
-
   mapFiltersFormElement.classList.add('map__filters--disabled');
-  mapFiltersFormElementsList.forEach((element) => {
-    element.setAttribute('disabled', 'disabled');
-  });
+  inputElementsList.forEach((element) => element.disabled = true);
 };
 
 const setPageActive = () => {
   advertFormElement.classList.remove('ad-form--disabled');
-  advertFormElementsList.forEach((element) => {
-    element.removeAttribute('disabled');
-  });
-
   mapFiltersFormElement.classList.remove('map__filters--disabled');
-  mapFiltersFormElementsList.forEach((element) => {
-    element.removeAttribute('disabled');
-  });
+  inputElementsList.forEach((element) => element.disabled = false);
 };
 
 export {setPageInactive, setPageActive};
