adf8741afde2a1bc9a4275eec8c2b3af28b1a869
irina.lebedeva <irina.lebedeva@travelline.ru>
2021-10-28 20:25:32 +0300

    Рефакторинг

--- js/ui-messages.js
+++ js/ui-messages.js
@@ -5,8 +5,8 @@ const ERROR_GET_DATA_CLASS_NAME = 'error--get-data';
 const ERROR_SEND_DATA_CLASS_NAME = 'error--send-data';
 const SUCCESS_SEND_DATA_CLASS_NAME = 'success--send-data';
 const bodyElement = document.querySelector('body');
-const errorFragment = document.querySelector('#error').content;
-const successFragment = document.querySelector('#success').content;
+const errorFragment = document.querySelector('#error').content.querySelector('.error');
+const successFragment = document.querySelector('#success').content.querySelector('.success');
 
 const closeMessage = (evt, selector, onKeyDownHandler) => {
   if (evt.type === 'keydown' && !isEscapeKey(evt)) {return;}
@@ -25,7 +25,7 @@ const closeSendDataErrorMessage = (evt) => closeMessage(evt, `.${ERROR_SEND_DATA
 const closeSendDataSuccessMessage = (evt) => closeMessage(evt, `.${SUCCESS_SEND_DATA_CLASS_NAME}`, closeSendDataSuccessMessage);
 
 const showGetDataErrorMessage = () => {
-  const errorNode = errorFragment.querySelector('.error').cloneNode(true);
+  const errorNode = errorFragment.cloneNode(true);
   errorNode.querySelector('.error__message').textContent = getMessage(DEFAULT_MESSAGES.getDataError);
   errorNode.querySelector('.error__button').remove();
   errorNode.classList.add(ERROR_GET_DATA_CLASS_NAME);
@@ -37,7 +37,7 @@ const showGetDataErrorMessage = () => {
 };
 
 const showSendDataErrorMessage = () => {
-  const errorNode = errorFragment.querySelector('.error').cloneNode(true);
+  const errorNode = errorFragment.cloneNode(true);
   const errorButton = errorNode.querySelector('.error__button');
 
   errorNode.classList.add(ERROR_SEND_DATA_CLASS_NAME);
@@ -50,7 +50,7 @@ const showSendDataErrorMessage = () => {
 };
 
 const showSendDataSuccessMessage = () => {
-  const successNode = successFragment.querySelector('.success').cloneNode(true);
+  const successNode = successFragment.cloneNode(true);
   successNode.classList.add(SUCCESS_SEND_DATA_CLASS_NAME);
 
   const successElement = bodyElement.appendChild(successNode);
